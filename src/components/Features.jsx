6ec3504f959341cfb0b47e46316606cf4d7d3e9a
Deepak-Maan <jattdevta1008@gmail.com>
2024-02-17 10:54:05 +0530

    Features section modified

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Clock, ClockPlus, Messages, ThreeLines } from './Icons'
 const Features = () => {
     return (
-        <div className='px-3 pt-[120px]'>
-            <div className='max-w-[1340px] mx-auto  px-3 sm:px-6 md:px-12 lg:px-[100px] pt-20 border border-[#FCFCFC] bg-[url("./assets/images/webp/bgservices.webp")] bg-no-repeat max-lg:bg-cover max-lg:bg-center lg:bg-Bgsize100 rounded-[32px] h-[810px] overflow-hidden '>
+        <div className='px-3 pt-12 md:pt-16 lg:pt-[120px]'>
+            <div className='max-w-[1340px] mx-auto  px-3 sm:px-6 md:px-12 lg:px-[100px] pt-7 sm:pt-20 border border-[#FCFCFC] bg-[url("./assets/images/webp/bgservices.webp")] bg-no-repeat max-lg:bg-cover max-lg:bg-center lg:bg-Bgsize100 rounded-[32px] h-[810px] overflow-hidden '>
                 <div className="sticky top-0">
-                    <h2 className='font-normal font-recharge leading-[50px] text-white text-[40px] text-center'>Unique features</h2>
-                    <p className=' text-center text-base font-normal font-poppins leading-[25px] max-w-[397px] mx-auto pt-4 max-md:pb-4 text-[#B7B7B7]'>ability to engage in natural, fluent conversations in over 15 languages,</p>
+                    <h2 className='font-normal font-recharge leading-8 sm:leading-[50px] text-white text-[25px] sm:text-[40px] text-center'>Unique features</h2>
+                    <p className=' text-center text-sm sm:text-base font-normal font-poppins leading-[25px] max-w-[397px] mx-auto pt-4 max-md:pb-4 text-[#B7B7B7]'>ability to engage in natural, fluent conversations in over 15 languages,</p>
                 </div>
-                <div className="grid lg:grid-cols-2  pt-20 max-h-[576px] my-scroll  overflow-y-scroll">
+                <div className="grid lg:grid-cols-2 pt-10 sm:pt-20 max-h-[650px] sm:max-h-[576px] my-scroll  overflow-y-scroll">
                     <div className='flex flex-col max-lg:items-center'>
                         <div className='max-w-[364px] bg-white px-6 pt-[30px] pb-11 rounded-2xl mb-5 sm:mb-10 md:mb-20 lg:mb-[175px]'>
                             <Clock />
