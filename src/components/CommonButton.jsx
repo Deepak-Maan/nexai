cc9ba236056f52cfe3203dd9cac50582d65ef76c
Deepak-Maan <jattdevta1008@gmail.com>
2024-02-19 10:12:06 +0530

    modified buttons

--- src/components/CommonButton.jsx
+++ src/components/CommonButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CommonButton = (props) => {
     return (
         <div>
-            <button className="bg-[#02CDCF] text-[#0B0A0A] font-recharge font-bold text-lg leading-[21px] rounded-xl py-[18px] px-8">{props.text}</button>
+            <button className="bg-[#02CDCF] hover:text-white duration-300 hover:outline  text-[#0B0A0A] font-recharge font-bold text-lg leading-[21px] rounded-xl py-[18px] px-8">{props.text}</button>
         </div>
     )
 }
